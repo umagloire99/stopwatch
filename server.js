acb7adaae604c5d13d28ad97c63616a19bbe787e
Ulrich Magloire <umagloire99@gmail.com>
2024-02-11 03:53:14 +0100

    Stopwatch

---
+++ server.js
@@ -0,0 +1,39 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const Pusher = require('pusher');
+const path = require('path');
+const NodeCache = require('node-cache');
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+const cache = new NodeCache();
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const pusher = new Pusher({
+    appId: '1754635',
+    key: '401f1d0958cb4236a24d',
+    secret: '60cb5966ae7087964d23',
+    cluster: 'mt1',
+    useTLS: true
+});
+
+app.post('/pusher/trigger/:eventName', (req, res) => {
+    const { value } = req.body;
+    const { eventName } = req.params;
+    
+    pusher.trigger('stopwatch-channel', eventName, {
+        value
+    });
+
+    res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
